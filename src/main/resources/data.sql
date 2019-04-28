7cbc0d3692a681e694df6541efc61784f5a3bd72
j.prakash.india@gmail.com <j.prakash.india@gmail.com>
2019-04-29 00:32:25 +0800

    added h2 db insertion command

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into jobs (job_id, job_details, job_name, job_status) values (1, 'LOH Job', 'LOH','Running');
+insert into jobs (job_id, job_details, job_name, job_status) values (2, 'NYH Job', 'LOH','Stoped');
+insert into jobs (job_id, job_details, job_name, job_status) values (3, 'BMH Job', 'BMH','Stoped');
